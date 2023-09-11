0a6bd0050f02e487b3536822915a8ed4994ba34a
Utkarsh Mall <utkarshmall13@gmail.com>
2023-09-11 12:54:39 -0400

    added models without lightning wrapper

--- src/utils/pretrained_checkpoints.py
+++ src/utils/pretrained_checkpoints.py
@@ -10,3 +10,16 @@ saved_ckpts = {
 	'r50_100k_caco': 'resnet50_caco_geo_100k_1000.ckpt',
 	'r50_1m_seco': 'resnet50_seco_geo_1m_200.ckpt',
 	'r50_1m_caco': 'resnet50_caco_geo_1m_200.ckpt'}
+
+saved_torch_ckpts = {
+	'r18_100k_seco':'resnet18_seco_geo_100k_1000.pth',
+	'r18_100k_moco': 'resnet18_moco_geo_100k_200.pth',
+	'r18_100k_teco': 'resnet18_teco_geo_100k_1000.pth',
+	'r18_100k_caco': 'resnet18_caco_geo_100k_1000.pth',
+	'r18_1m_seco': 'resnet18_seco_geo_1m_200.pth',
+	'r18_1m_caco': 'resnet18_caco_geo_1m_200.pth',
+	'r50_100k_moco': 'resnet50_moco_geo_100k_200.pth',
+	'r50_100k_seco': 'resnet50_seco_geo_100k_1000.pth',
+	'r50_100k_caco': 'resnet50_caco_geo_100k_1000.pth',
+	'r50_1m_seco': 'resnet50_seco_geo_1m_200.pth',
+	'r50_1m_caco': 'resnet50_caco_geo_1m_200.pth'}
